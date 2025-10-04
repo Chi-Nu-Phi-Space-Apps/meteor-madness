105f2a59c319ada61508b9892b3c850a24aea18b
Randy Moore <randy2@clemson.edu>
2025-10-04 13:23:53 -0400

    Added user inputs if needed so its not static.

--- data_query.py
+++ data_query.py
@@ -4,9 +4,9 @@ import json
 
 url = "https://api.nasa.gov/neo/rest/v1/feed"
 params = {
-    "start_date": "2025-9-27",
-    "end_date": "2025-10-3", 
-    "api_key": "X88I4yhUbfhmxBvBbNCamEmx4g2IxsHaqly0JPKJ"
+    "start_date": input("Enter start date: ") or "2025-9-27",
+    "end_date": input("Enter end date: ") or "2025-10-3", 
+    "api_key": input("Enter API key: ")
 }
 
 response = requests.get(url, params=params)
@@ -14,7 +14,7 @@ response = requests.get(url, params=params)
 if response.status_code == 200:
     data = response.json()
 
-    with open("asteroid_data.json", "w") as f:
+    with open("data/asteroid_data.json", "w") as f:
         json.dump(data, f, indent=2)
 
     print("Successfully read from api and created json with data.")
