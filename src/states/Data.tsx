ffffde26d3875539769fa5ae4943abd2bb36bd02
Nick Rodriguez <68359871+nicolas377@users.noreply.github.com>
2025-10-05 01:04:30 -0400

    got data view ready for lat/long data

--- src/states/Data.tsx
+++ src/states/Data.tsx
@@ -1,16 +1,72 @@
 import React, { useEffect } from "react";
 import type { Data } from "../types";
 
-export default function DataViewer({ data, selectedAsteroid }: { data: Data, selectedAsteroid: string }) {
-  const ref = React.useRef(null);
-  
+function OverlayOnImage({
+  src,
+  x,
+  y,
+  children,
+}: {
+  src: string;
+  x: number;
+  y: number;
+  children?: React.ReactNode;
+}) {
+  return (
+    <div
+      style={{
+        position: "relative",
+        width: "100%",
+        display: "inline-block",
+      }}
+    >
+      <img
+        src={src}
+        alt=""
+        style={{
+          width: "100%",
+          height: "auto",
+          display: "block",
+        }}
+      />
+      <div
+        style={{
+          position: "absolute",
+          top: `${y}%`,
+          left: `${x}%`,
+          transform: "translate(-50%, -50%)",
+          pointerEvents: "none", // let clicks go through
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default function DataViewer({
+  data,
+  selectedAsteroid,
+}: {
+  data: Data;
+  selectedAsteroid: string;
+}) {
+  const ref = React.useRef<HTMLImageElement>(null);
+  const [xPercent, setXPercent] = React.useState<number>(0);
+  const [yPercent, setyPercent] = React.useState<number>(0);
+
+  // TODO: calculate percents from lat/long
+
   useEffect(() => {
-    
+    console.log(ref);
+    console.log(`width: ${ref.current?.width}, height ${ref.current?.height}`);
   }, []);
 
   return (
     <div id="gameData">
-      <img src="./worldMap.jpg" alt="a world map" ref={ref} />
+      <OverlayOnImage src="./worldMap.jpg" x={xPercent} y={yPercent}>
+        <p style={{ font: "caption", fontSize: "1.5rem", color: "red" }}>x</p>
+      </OverlayOnImage>
     </div>
   );
 }
