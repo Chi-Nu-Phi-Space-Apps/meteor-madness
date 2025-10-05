e06e4d0fb1f606a5c7319f059af1d4b9b550253d
Grant Cooper <gacoope@clemson.edu>
2025-10-05 01:07:57 -0400

    Changed text to be a description instead of instructions

--- src/Instructions.tsx
+++ src/Instructions.tsx
@@ -7,8 +7,8 @@ export default function Instructions() {
         Have you wondered what asteroid impacts could actually result in?<br />
         Try your hand at a GeoGuessr-style game complete with real data!
       </p>
-      <h3>Instructions</h3>
-      <p>Blah blah blah super cool game</p>
+      <h3>About Our Project</h3>
+      <p>We used mathplotlib as well as a variety of complex math equations to find an approximation of where the asteroids will impact using the data from the Nasa NEO API. We took the coordinates from our algorithm and mapped it to a map of the world in order to mark down where the meteor will impact.</p>
     </div>
   );
 }
