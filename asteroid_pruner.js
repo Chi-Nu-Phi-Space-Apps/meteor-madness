171399d79fbf2e36c3018f55b18a274061433165
Nick Rodriguez <68359871+nicolas377@users.noreply.github.com>
2025-10-05 10:49:04 -0400

    Data typing and regen

--- asteroid_pruner.js
+++ asteroid_pruner.js
@@ -14,13 +14,13 @@ for (const neo of Object.values(originalData.near_earth_objects).flat()) {
     name: neo.name,
     nasa_jpl_url: neo.nasa_jpl_url,
     absolute_magnitude_h: neo.absolute_magnitude_h,
-    estimated_diameter: neo.estimated_diameter,
     is_potentially_hazardous_asteroid: neo.is_potentially_hazardous_asteroid,
     asteroid_mass: simData.asteroid_mass[simNeoId],
     impact_velocity_kps: simData.impact_velocity_kps[simNeoId],
     impact_energy_mt: simData.impact_energy_mt[simNeoId],
     impact_lat: simData.impact_lat[simNeoId],
     impact_lon: simData.impact_lon[simNeoId],
+    diameter_km: simData.diameter[simNeoId],
   });
 }
 
