65bc69a759a71d3157179814ffa4d86e6309e8d5
Grant Cooper <gacoope@clemson.edu>
2025-10-05 01:20:38 -0400

    More official

--- src/Instructions.tsx
+++ src/Instructions.tsx
@@ -5,7 +5,7 @@ export default function Instructions() {
     <div>
       <p>
         Have you wondered what asteroid impacts could actually result in?<br />
-        Try your hand at a GeoGuessr-style game complete with real data!
+        Try your hand at a GeoGuessr-style game complete with official NASA data!
       </p>
       <h3>About Our Project</h3>
       <p>We used mathplotlib as well as a variety of complex math equations to find an approximation of where the asteroids will impact using the data from the Nasa NEO API. We took the coordinates from our algorithm and mapped it to a map of the world in order to mark down where the meteor will impact.</p>
