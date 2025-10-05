761d6985a6e2ba35c7128684905057d8772f4f1d
Lee Truong <lvtruon@clemson.edu>
2025-10-05 11:36:47 -0400

    aaa

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <p>Have you wondered what asteroid impacts could actually result in?</p>
       <h3>About Our Project</h3>
       <p>We used a variety of complex math equations to find an approximation of where the asteroids will impact as well as impact energy, impact speed, and more using the data from the Nasa NEO API. We took the coordinates from our algorithm and mapped it to a map of the world in order to mark down where the meteor will impact.</p>
-      <img src="./photos/CNPlogo_bgRemoved.png" alt="Chi Nu Phi Official Crest" className="center"></img>
+      <img src="./photos/CNP_BGR2.png" alt="Chi Nu Phi Official Crest" className="center"></img>
     </div>
   );
 }
