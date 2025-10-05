309ef724ad5d759b15eaf85e794f4d145bea4f70
Grant Cooper <gacoope@clemson.edu>
2025-10-04 22:54:44 -0400

    Formating

--- src/Instructions.tsx
+++ src/Instructions.tsx
@@ -4,8 +4,8 @@ export default function Instructions() {
   return (
     <div>
       <p>
-        Have you wondered what asteroid impacts could actually result in? Try
-        your hand at a GeoGuessr-style game complete with real data!
+        Have you wondered what asteroid impacts could actually result in?<br />
+        Try your hand at a GeoGuessr-style game complete with real data!
       </p>
       <h3>Instructions</h3>
       <p>Blah blah blah super cool game</p>
